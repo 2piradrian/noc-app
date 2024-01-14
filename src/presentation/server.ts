71afdcd58fc01342a6af7b18db42eca8e739e130
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-01-14 15:09:50 -0300

    Add cron dependency to package.json

---
+++ src/presentation/server.ts
@@ -0,0 +1,11 @@
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+	public static start() {
+		console.log("Server started");
+
+		CronService.createJob("*/5 * * * * *", () => {
+			console.log("Cron job running");
+		});
+	}
+}
